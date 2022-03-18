395ad9ec77161ddba58ae94bc928836bbf0d40dd
PengShuai1995 <1447902621@qq.com>
2022-03-18 11:05:07 +0800

    3_19

--- Step3/tutorial.cxx
+++ Step3/tutorial.cxx
@@ -1,17 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <cmath>
+#include <iostream>
+#include <string>
 
-int main(int argc,char *argv[])
+#include "TutorialConfig.h"
+//should we include the MathFunctions header?
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
+int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        fprintf(stdout,"Usage: %s number\n",argv[0]);
+        std::cout<<argv[0]<<" Version "<<Tutorial_VERSION_MAHOR<<"."
+                <<Tutorial_BERSION_MINOR<<std::endl;
+        std::cout<<"Usage: "<<argv[0]<<" number"<<std::endl;
         return 1;
     }
-    double inputValue = atof(argv[1]);
-    double outputValue = sqrt(inputValue);
-    fprintf(stdout,"The square root of %g is %g\n",
-            inputValue,outputValue);
-    return 0;
+    const double inputValue = std::stod(argv[1]);
+    
+    #ifdef USEMATH
+     const double outputValue = sqrt(intputValue);
+    #else
+     const double outputValue = sqrt(inputValue);
+    #endif
+     std::cout<<"The square root of "<<inputValue<<" is "<<outputValue
+              <<std::endl;
+   return 0;
+
 }
